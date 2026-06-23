a5ea5607f6c6a252f8a49331361bf9f33fa4ed17
tochukwu <okeakputochukwu9@gmail.com>
2026-06-23 18:42:44 -0400

    resend env details added

--- backend/server.ts
+++ backend/server.ts
@@ -4,13 +4,18 @@ import { init_socket } from "./src/socket.ts";
 import app from "./app.ts";
 import { load_model } from "./ml/predict.ts";
 
+console.log(1);
 const http_server = createServer(app);
 
 // attach socket.io to the http server
+console.log(2);
 init_socket(http_server);
 
 // load TF model
+console.log(3);
 load_model();
+
+console.log(4);
 
 const PORT = process.env.PORT || 5000;
 http_server.listen(PORT, () => {
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,9 +2,13 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const connection_string = process.env.DATABASE_URL;
+
+if (!connection_string) {
+  throw new Error("DATABASE_URL is not set in environment variables");
+}
+
+const adapter = new PrismaPg({ connectionString: connection_string });
 
 const prisma = new PrismaClient({ adapter });
 
